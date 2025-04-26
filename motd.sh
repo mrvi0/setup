bf6ff5854ab4c9a484e6470955e0dee2434f23cc
Vi <support@b4dcat.ru>
2025-04-27 01:53:45 +0300

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Удаляем права на выполнение у всех файлов внутри /etc/update-motd.d/
+sudo chmod -x /etc/update-motd.d/*
+
+# Создаем файл 01-custom с необходимым содержимым
+sudo bash -c 'cat > /etc/update-motd.d/01-custom << "EOF"
+#!/bin/bash
+
 # Цвета
 tcLtG="\033[00;37m" # LIGHT GRAY
 tcDkG="\033[01;30m" # DARK GRAY
@@ -28,10 +35,10 @@ upHours=$((uptime_secs/60/60%24))
 upMins=$((uptime_secs/60%60))
 
 # Использование ресурсов
-SYS_LOADS=$(cut -d' ' -f1 /proc/loadavg) # Используем cut вместо awk для простоты
+SYS_LOADS=$(cut -d' ' -f1 /proc/loadavg)
 MEMORY_USED=$(free -b | awk '/^Mem:/ {printf "%.2f", $3/$2 * 100.0}')
-SWAP_USED=$(free -b | awk '/^Swap:/ {if ($2 > 0) printf "%.2f", $3/$2 * 100.0; else printf "0.00";}') # Проверка деления на ноль
-NUM_PROCS=$(ps -e --no-headers | wc -l) # -e --no-headers надежнее чем ps aux | wc -l
+SWAP_USED=$(free -b | awk '/^Swap:/ {if ($2 > 0) printf "%.2f", $3/$2 * 100.0; else printf "0.00";}')
+NUM_PROCS=$(ps -e --no-headers | wc -l)
 
 # IP-адрес и hostname
 IPADDRESS=$(hostname -I | cut -d' ' -f1)
@@ -39,45 +46,35 @@ IPADDRESS=$(hostname -I | cut -d' ' -f1)
 
 HOSTNAME_FQDN=$(hostname -f)
 LSB_RELEASE=$(lsb_release -s -d)
-KERNEL_INFO=$(uname -s) # Kernel name
-KERNEL_RELEASE=$(uname -r) # Kernel release
-KERNEL_ARCH=$(uname -m) # Kernel architecture
-CURRENT_USERS=$(who | wc -l) # who надежнее, чем users | wc -w для подсчета сессий
+KERNEL_INFO=$(uname -s)
+KERNEL_RELEASE=$(uname -r)
+KERNEL_ARCH=$(uname -m)
+CURRENT_USERS=$(who | wc -l)
 CURRENT_DATE=$(date)
 
-# Создание нового MOTD
-MOTD_CONTENT="
-${tcDkG}=================================================================${tcRESET}
-${tcLtG}Привет тебе этим ${TIME} !${tcRESET}
-${tcLtP}  ____    _  _     _____     _____              _______ ${tcRESET}
-${tcLtP} |  _ \\  | || |   |  __ \\   / ____|     /\\     |__   __|${tcRESET}
-${tcLtP} | |_) | | || |_  | |  | | | |         /  \\       | |   ${tcRESET}
-${tcLtP} |  _ <  |__   _| | |  | | | |        / /\\ \\      | |   ${tcRESET}
-${tcLtP} | |_) |    | |   | |__| | | |____   / ____ \\     | |   ${tcRESET}
-${tcLtP} |____/     |_|   |_____/   \\_____| /_/    \\_\\    |_|   ${tcRESET}
-${tcDkG}=================================================================${tcRESET}
-${tcLtG} - Hostname      : ${tcW}${HOSTNAME_FQDN}${tcRESET}
-${tcLtG} - IP Address    : ${tcW}${IPADDRESS}${tcRESET}
-${tcLtG} - Release       : ${tcW}${LSB_RELEASE}${tcRESET}
-${tcLtG} - Kernel        : ${tcW}${KERNEL_INFO} ${KERNEL_RELEASE} ${KERNEL_ARCH}${tcRESET}
-${tcLtG} - Users         : ${tcW}Currently ${CURRENT_USERS} user(s) logged on${tcRESET}
-${tcLtG} - Server Time   : ${tcW}${CURRENT_DATE}${tcRESET}
-${tcLtG} - System load   : ${tcW}${SYS_LOADS}${tcRESET} / ${tcW}${NUM_PROCS}${tcRESET} processes running
-${tcLtG} - Memory used % : ${tcW}${MEMORY_USED}%${tcRESET}
-${tcLtG} - Swap used %   : ${tcW}${SWAP_USED}%${tcRESET}
-${tcLtG} - System uptime : ${tcW}${upDays} days ${upHours} hours ${upMins} minutes${tcRESET}
-${tcW} Нужно добавить инструкцию ${tcLtGRN}./инструкция.sh${tcRESET}
-${tcDkG}=================================================================${tcRESET}
-"
+# Вывод динамического MOTD
+echo -e "${tcDkG}=================================================================${tcRESET}"
+echo -e "${tcLtG}Привет тебе этим ${TIME} !${tcRESET}"
+echo -e "${tcLtP}  ____    _  _     _____     _____              _______ ${tcRESET}"
+echo -e "${tcLtP} |  _ \\  | || |   |  __ \\   / ____|     /\\     |__   __|${tcRESET}"
+echo -e "${tcLtP} | |_) | | || |_  | |  | | | |         /  \\       | |   ${tcRESET}"
+echo -e "${tcLtP} |  _ <  |__   _| | |  | | | |        / /\\ \\      | |   ${tcRESET}"
+echo -e "${tcLtP} | |_) |    | |   | |__| | | |____   / ____ \\     | |   ${tcRESET}"
+echo -e "${tcLtP} |____/     |_|   |_____/   \\_____| /_/    \\_\\    |_|   ${tcRESET}"
+echo -e "${tcDkG}=================================================================${tcRESET}"
+echo -e "${tcLtG} - Hostname      : ${tcW}${HOSTNAME_FQDN}${tcRESET}"
+echo -e "${tcLtG} - IP Address    : ${tcW}${IPADDRESS}${tcRESET}"
+echo -e "${tcLtG} - Release       : ${tcW}${LSB_RELEASE}${tcRESET}"
+echo -e "${tcLtG} - Kernel        : ${tcW}${KERNEL_INFO} ${KERNEL_RELEASE} ${KERNEL_ARCH}${tcRESET}"
+echo -e "${tcLtG} - Users         : ${tcW}Currently ${CURRENT_USERS} user(s) logged on${tcRESET}"
+echo -e "${tcLtG} - Server Time   : ${tcW}${CURRENT_DATE}${tcRESET}"
+echo -e "${tcLtG} - System load   : ${tcW}${SYS_LOADS}${tcRESET} / ${tcW}${NUM_PROCS}${tcRESET} processes running"
+echo -e "${tcLtG} - Memory used % : ${tcW}${MEMORY_USED}%${tcRESET}"
+echo -e "${tcLtG} - Swap used %   : ${tcW}${SWAP_USED}%${tcRESET}"
+echo -e "${tcLtG} - System uptime : ${tcW}${upDays} days ${upHours} hours ${upMins} minutes${tcRESET}"
+echo -e "${tcLtR} USE ./vps_info.sh${tcRESET}"
+echo -e "${tcDkG}=================================================================${tcRESET}"
+EOF'
 
-# Удаляем права на выполнение у всех файлов в /etc/update-motd.d/
-sudo chmod -x /etc/update-motd.d/*
-
-# Записываем в файл MOTD
-echo -e "$MOTD_CONTENT" | sudo tee /etc/update-motd.d/01-custom > /dev/null
-
-# Даем права на выполнение для нового MOTD
+# Даем права на исполнение для нового файла
 sudo chmod +x /etc/update-motd.d/01-custom
-
-# Отображаем на экране
-echo -e "$MOTD_CONTENT"
